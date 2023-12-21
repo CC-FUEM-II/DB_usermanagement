2ec7d3dfcad01243c4ec8b6135fc775083a6083f
hesse65 <152978535+hesse65@users.noreply.github.com>
2023-12-21 12:59:40 +0100

    Update notepad.sql

--- sql/notepad.sql
+++ sql/notepad.sql
@@ -7,19 +7,20 @@ SHOW TABLES;
 SELECT * FROM mydb.employees;
 SELECT * FROM mydb.countries;
 
-
+-- Wer sind wir in MySQL?  
 SELECT user();
+-- @root:
 
 -- user-Tabelle
 SHOW DATABASES;
 USE mysql;
 SHOW TABLES;
-
 DESCRIBE mysql.user;
 
 SELECT 
-     user, authentication_string 
-     FROM mysql.user;
+	user, 
+	authentication_string 
+FROM mysql.user;
 
 -- User anlegen 
 CREATE USER if NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin';
@@ -34,42 +35,67 @@ FROM mysql.user;
 -- Berechtigungen zuweisen
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
 
--- Reload der Berechtigungen
+-- Reload (update) der Berechtigungen
 FLUSH PRIVILEGES;
 
 -- als Root abmelden
 EXIT
 
--- als Admin anmelden (unteter Linux)
+-- Unter LINUX als admin in MySQL anmelden / @admin
 mysql -u admin -p
 
 
--- User-Rechte anzeigen
+-- User-Rechte von admin anzeigen -- @admin: welche Rechte habe ich?
 SHOW GRANTS FOR 'admin'@'localhost';
 
--- Anlegen allg. USER
+-- Anlegen allg. USER USER max mit PW = max
 CREATE USER 'max'@'localhost' IDENTIFIED BY 'max';
 
 -- Alle Rechte anzeigen
 SHOW PRIVILEGES;
 
--- Rechte zuweisen
-SHOW PRIVILEGES;GRANT
+-- Rechte anzeigen
+SHOW PRIVILEGES;
+
+ -- @admin: Rechte hinzufügen
+GRANT
   SHOW DATABASES, SELECT
   ON *.*
   TO 'max'@'localhost';
   
-  
+-- login als max
+-- @max: OK  
 SHOW DATABASES;
 SELECT * FROM mydb.employees;
 SELECT * FROM mydb.countries;
 
--- Rechte zuweisen
+-- @max: keine Berechtigung zum Löschen
+DROP TABLE mydb.employees; 
+
+-- Rechte zuweisen (Lösche)
 SHOW PRIVILEGES;
 GRANT
   DROP
   ON mydb.*
   TO 'max'@'localhost';
+
+-- @max: OK
+DROP TABLE mydb.employees; 
+
+-- @admin: Löschberechtigung in mydb für user max wieder entziehen
+REVOKE
+	DROP
+	ON mydb.*
+	FROM 'max'@'localhost';
+FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'max'@'localhost';
+
+-- @max: keine Berechtigung mehr 
+DROP TABLE mydb.countries; 
+
+-- @admin: User max löschen
+DROP USER 'max'@'localhost';
   
   -- Rechte entfernen
 REVOKE DROP ON mydb.* TO 'max'@'localhost';
